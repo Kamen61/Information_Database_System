e6150704b5d4a886d893f98b9ea4abeb9499261c
Kamen <canen321@gmail.com>
2022-11-20 20:11:30 +0300

    done

--- search_and_return_module.py
+++ search_and_return_module.py
@@ -1,7 +1,7 @@
-# import sqlite3
+import sqlite3
 # Создание БД
-# conn = sqlite3.connect('taxi_drivers.db')
-# cur = conn.cursor()
+conn = sqlite3.connect('taxi_drivers.db')
+cur = conn.cursor()
 
 # cur.execute("""CREATE TABLE drivers(
 #    userid INT PRIMARY KEY,
@@ -14,7 +14,6 @@
 # """)
 # conn.commit()
 
-
 # drivers = [
 #   ('1', 'Stephanie Stewart', '35659','4,8','Mazda','True', ''),
 #   ('2', 'Sincere Sherman', '30819','3,9','Lada','False', ''),
@@ -26,10 +25,31 @@
 #   ('8', 'Gaige Summers', '38515','4,9','Mazda','True', ''),
 #   ('9', 'Andrea Snow', '37086','4,8','Kia','False', '')
 # ]
-#
 # cur.executemany("INSERT INTO drivers VALUES(?, ?, ?, ?, ?, ?, ?);", drivers)
 # conn.commit()
 
-# cur.execute("SELECT * FROM drivers;")
-# all_results = cur.fetchall()
-# print(all_results)
+
+
+def search(list_parametrs):
+    list_keys=[]
+    list_values=[]
+    all_columns = ["PRIMARY KEY", "full_name", "identification_number", "rating", "auto", "fines", "last_modified"]
+    for i,j in enumerate(list_parametrs):
+        if j!='-1':
+            list_keys.append(all_columns[i])
+            list_values.append(j)
+    sql_request = ''
+    if len(list_keys) > 1:
+        len_list=len(list_keys)
+        count=0
+        while len_list:
+            sql_request+='{}="{}" AND '.format(list_keys[count],list_values[count])
+            len_list-=1
+            count+=1
+        sql_request=sql_request[:-4]
+        cur.execute(f"""SELECT * FROM drivers WHERE {sql_request}""")
+    else:
+        sql_request += '{}="{}"'.format(list_keys[0], list_values[0])
+        cur.execute(f"""SELECT * FROM drivers WHERE {sql_request}""")
+    return cur.fetchall()
+
