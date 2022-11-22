2aea6de1abe55b37b2f3625d8184fe67f89a20c7
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-11-22 17:27:31 +0300

    Update change_module.py
    Обновлена функция изменения данных

--- change_module.py
+++ change_module.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('taxi_drivers.db')
 cur = conn.cursor()
 
 def sql_update(list_param):    # принимает  входящий спискок изменений 
-    all_columns = ["PRIMARY KEY", "full_name", "identification_number", "rating", "auto", "fines", "last_modified"]
+    all_columns = ["userid", "full_name", "identification_number", "rating", "auto", "fines", "last_modified"]
     now = datetime.datetime.now()
     time = now.strftime("%d-%m-%Y %H:%M")
 
@@ -15,9 +15,11 @@ def sql_update(list_param):    # принимает  входящий списк
             print('Not value')
         else:                      # изменяет данные в соответствуещем поле
             cur.execute(f' UPDATE drivers SET {all_columns[i]} = "{list_param[i]}" where userid = {list_param[0]}')
+            conn.commit()
 
     cur.execute(f' UPDATE drivers SET {all_columns[6]} = "{time}" where userid = {list_param[0]}')  #вносит дату и время
-
+    conn.commit()
     return cur.execute(f'SELECT * FROM drivers where userid = {list_param[0]} ')     # возвращает обновленные данные
+
 
 
